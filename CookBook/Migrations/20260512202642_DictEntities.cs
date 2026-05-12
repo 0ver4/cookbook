2c7a8288ae690d9cf1c22c5074c3377e056af6a9
Julia Klepadło <202800721+0ver4@users.noreply.github.com>
2026-05-12 22:37:20 +0200

    Więcej modeli

---
+++ CookBook/Migrations/20260512202642_DictEntities.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CookBook.Migrations
+{
+    /// <inheritdoc />
+    public partial class DictEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Allergens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Allergens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Units",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Units", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Allergens",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Gluten" },
+                    { 2, "Orzechy" },
+                    { 3, "Mleko" },
+                    { 4, "Jaja" },
+                    { 5, "Ryby" },
+                    { 6, "Skorupiaki" },
+                    { 7, "Pomidory" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Zupa" },
+                    { 2, "Danie główne" },
+                    { 3, "Deser" },
+                    { 4, "Śniadanie" },
+                    { 5, "Obiad" },
+                    { 6, "Kolacja" },
+                    { 7, "Przekąska" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "DifficultyLevels",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Łatwy" },
+                    { 2, "Średni" },
+                    { 3, "Trudny" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tags",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Wegetariańskie" },
+                    { 2, "Wegańskie" },
+                    { 3, "Bezglutenowe" },
+                    { 4, "Dla dzieci" },
+                    { 5, "Szybkie" },
+                    { 6, "Na imprezę" },
+                    { 7, "Słodkie" },
+                    { 8, "Słone" },
+                    { 9, "Ostre" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Units",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "gram" },
+                    { 2, "mililitr" },
+                    { 3, "sztuka" },
+                    { 4, "szklanka" },
+                    { 5, "łyżeczka" },
+                    { 6, "łyżka" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Allergens_Name",
+                table: "Allergens",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Name",
+                table: "Categories",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Name",
+                table: "Tags",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Units_Name",
+                table: "Units",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Allergens");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "Units");
+
+            migrationBuilder.DeleteData(
+                table: "DifficultyLevels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DifficultyLevels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DifficultyLevels",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
