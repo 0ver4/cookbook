c6714a9bd2476696813840396f46bb58e358819f
Julia Klepadło <202800721+0ver4@users.noreply.github.com>
2026-05-12 22:37:20 +0200

    Więcej modeli

---
+++ CookBook/Models/IngredientAllergen.cs
@@ -0,0 +1,10 @@
+namespace CookBook.Models;
+
+public class IngredientAllergen
+{
+    public int IngredientId { get; set; }
+    public Ingredient Ingredient { get; set; } = null!;
+    
+    public int AllergenId { get; set; }
+    public Allergen Allergen { get; set; } = null!;
+}
