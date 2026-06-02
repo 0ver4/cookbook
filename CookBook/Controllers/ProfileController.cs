02aebd780394f4167e0ddb908209142cd8c61579
Anna Karpińska <ania.k05@wp.pl>
2026-06-02 18:22:14 +0200

    Dodanie profilu uzytkownika i formularza edycji danych

---
+++ CookBook/Controllers/ProfileController.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using CookBook.Models;
+
+namespace CookBook.Controllers;
+
+[Authorize] // tylko zalogowani maja dostep
+public class ProfileController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public ProfileController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        // dane zalogowanego
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return NotFound("Nie znaleziono użytkownika.");
+        }
+
+        // rola uzytkownika
+        var roles = await _userManager.GetRolesAsync(user);
+        ViewBag.UserRole = roles.FirstOrDefault() ?? "Brak roli";
+
+        return View(user);
+    }
+
+    // formularz edycji
+    [HttpGet]
+    public async Task<IActionResult> Edit()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return NotFound("Nie znaleziono użytkownika.");
+        }
+
+        return View(user);
+    }
+
+    // aktualizacja danych
+    [HttpPost]
+    [ValidateAntiForgeryToken] 
+    public async Task<IActionResult> Edit(string firstName, string lastName)
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return NotFound("Nie znaleziono użytkownika.");
+        }
+
+        user.FirstName = firstName;
+        user.LastName = lastName;
+
+        // aktualizacja uzytkownika
+        var result = await _userManager.UpdateAsync(user);
+        if (result.Succeeded)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        foreach (var error in result.Errors)
+        {
+            ModelState.AddModelError(string.Empty, error.Description);
+        }
+
+        return View(user);
+    }
+}
