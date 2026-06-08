e020836f54075fc120c30b90df9aaa1938bd2223
n3oney <neo@neoney.dev>
2026-06-08 23:04:16 +0200

    image data w bazie

---
+++ CookBook/Migrations/20260608210202_ImageBlobStorage.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookBook.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageBlobStorage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Images_Url",
+                table: "Images");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Images",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContentType",
+                table: "Images",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Data",
+                table: "Images",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentType",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "Data",
+                table: "Images");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Images",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_Url",
+                table: "Images",
+                column: "Url",
+                unique: true);
+        }
+    }
+}
--- CookBook/Models/Image.cs
+++ CookBook/Models/Image.cs
@@ -6,8 +6,15 @@ public class Image
 {
     public int Id { get; set; }
 
-    [Required, MaxLength(500)]
-    public string Url { get; set; } = null!;
+    // Wgrane pliki: zawartość trzymana w bazie (blob), synchronizuje się między środowiskami
+    public byte[]? Data { get; set; }
+
+    [MaxLength(100)]
+    public string? ContentType { get; set; }
+
+    // Zewnętrzne obrazki (np. ikony reakcji): adres zamiast blobu
+    [MaxLength(500)]
+    public string? Url { get; set; }
 
     public int UploadedById { get; set; }
     public ApplicationUser UploadedBy { get; set; } = null!;
