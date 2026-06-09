9e3d2de7350a1a8b6689517491705d9e20044d13
Anna Karpińska <ania.k05@wp.pl>
2026-06-09 14:50:08 +0200

    Dodanie generowania przepisów do PDF oraz przycisku pobierania

--- CookBook/Controllers/RecipeController.cs
+++ CookBook/Controllers/RecipeController.cs
@@ -5,6 +5,8 @@ using CookBook.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
 
 namespace CookBook.Controllers;
 
@@ -27,7 +29,6 @@ public class RecipeController : Controller
     private int CurrentUserId => int.Parse(_userManager.GetUserId(User)!);
     private bool IsModerator => User.IsInRole("Moderator");
 
-    // GET: /Recipe
     [AllowAnonymous]
     public async Task<IActionResult> Index([FromQuery] RecipeQuery query)
     {
@@ -35,7 +36,6 @@ public class RecipeController : Controller
         return View(vm);
     }
 
-    // GET: /Recipe/Details/5
     [AllowAnonymous]
     public async Task<IActionResult> Details(int id)
     {
@@ -55,7 +55,6 @@ public class RecipeController : Controller
         return View(recipe);
     }
 
-    // GET: /Recipe/Create
     [Authorize]
     public async Task<IActionResult> Create()
     {
@@ -68,7 +67,6 @@ public class RecipeController : Controller
         return View(vm);
     }
 
-    // POST: /Recipe/Create
     [HttpPost]
     [Authorize]
     [ValidateAntiForgeryToken]
@@ -92,7 +90,6 @@ public class RecipeController : Controller
         return RedirectToAction(nameof(Details), new { id = recipeId });
     }
 
-    // GET: /Recipe/Edit/5
     [Authorize]
     public async Task<IActionResult> Edit(int id)
     {
@@ -106,7 +103,6 @@ public class RecipeController : Controller
         return View(vm);
     }
 
-    // POST: /Recipe/Edit/5
     [HttpPost]
     [Authorize]
     [ValidateAntiForgeryToken]
@@ -130,7 +126,6 @@ public class RecipeController : Controller
         return RedirectToAction(nameof(Details), new { id = vm.Id });
     }
 
-    // GET: /Recipe/Delete/5
     [Authorize]
     public async Task<IActionResult> Delete(int id)
     {
@@ -144,7 +139,6 @@ public class RecipeController : Controller
         return View(recipe);
     }
 
-    // POST: /Recipe/Delete/5
     [HttpPost, ActionName("Delete")]
     [Authorize]
     [ValidateAntiForgeryToken]
@@ -201,6 +195,69 @@ public class RecipeController : Controller
         }
 
         return RedirectToAction(nameof(Details), new { id = recipeId });
+    }
+
+    [AllowAnonymous]
+    public async Task<IActionResult> DownloadPdf(int id)
+    {
+        var recipe = await _service.GetDetailsAsync(id);
+        if (recipe is null)
+            return NotFound();
+
+        var document = Document.Create(container =>
+        {
+            container.Page(page =>
+            {
+                page.Size(PageSizes.A4);
+                page.Margin(40); 
+                page.PageColor(Colors.White);
+                page.DefaultTextStyle(x => x.FontSize(11).FontColor(Colors.Grey.Darken3));
+
+                page.Header().Text(recipe.Name)
+                    .SemiBold().FontSize(24).FontColor(Colors.Indigo.Medium);
+
+                page.Content().PaddingVertical(20).Column(column =>
+                {
+                    column.Spacing(15);
+
+                    column.Item().Text($"Autor: {recipe.AuthorName}  |  Trudność: {recipe.DifficultyName}  |  Data: {recipe.CreatedAt.ToLocalTime().ToString("dd.MM.yyyy")}")
+                        .Italic().FontColor(Colors.Grey.Medium);
+
+                    if (!string.IsNullOrWhiteSpace(recipe.Description))
+                    {
+                        column.Item().Text(recipe.Description).LineHeight(1.3f);
+                    }
+
+                    column.Item().LineHorizontal(1).LineColor(Colors.Grey.Lighten2);
+
+                    column.Item().Text("Składniki:").Bold().FontSize(14).FontColor(Colors.Indigo.Darken2);
+                    foreach (var ing in recipe.Ingredients)
+                    {
+                        column.Item().Text($"• {ing.IngredientName} - {ing.Amount.ToString("0.##")} {ing.UnitName}");
+                    }
+
+                    column.Item().LineHorizontal(1).LineColor(Colors.Grey.Lighten2);
+
+                    column.Item().Text("Sposób przygotowania:").Bold().FontSize(14).FontColor(Colors.Indigo.Darken2);
+                    foreach (var step in recipe.Steps.OrderBy(s => s.Order))
+                    {
+                        column.Item().Text($"{step.Order}. {step.Content}").LineHeight(1.2f);
+                    }
+                });
+
+                page.Footer().AlignCenter().Text(x =>
+                {
+                    x.Span("Strona ");
+                    x.CurrentPageNumber();
+                });
+            });
+        });
+
+        var pdfBytes = document.GeneratePdf();
+        string safeFileName = recipe.Name.Replace(" ", "_") + "_przepis.pdf";
+        
+        // Zwraca plik jako załącznik, co wymusi natychmiastowe pobranie w przeglądarce
+        return File(pdfBytes, "application/pdf", safeFileName);
     }
 
     private async Task<bool> UserCanModify(int recipeId)
