0e2d576647c79fcc072f8f8c1ee107887ddf8a0a
Anna Karpińska <ania.k05@wp.pl>
2026-06-09 13:44:13 +0200

    Fixes #17 and Fixes #18

--- CookBook/Controllers/ProfileController.cs
+++ CookBook/Controllers/ProfileController.cs
@@ -1,18 +1,22 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using CookBook.Models;
+using CookBook.Data;
 
 namespace CookBook.Controllers;
 
-[Authorize] // tylko zalogowani 
+[Authorize]
 public class ProfileController : Controller
 {
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly CookBookContext _context; 
 
-    public ProfileController(UserManager<ApplicationUser> userManager)
+    public ProfileController(UserManager<ApplicationUser> userManager, CookBookContext context)
     {
         _userManager = userManager;
+        _context = context;
     }
 
     public async Task<IActionResult> Index()
@@ -22,14 +26,28 @@ public class ProfileController : Controller
         {
             return NotFound("Nie znaleziono użytkownika.");
         }
-        
+
+        // Pobieramy rolę
         var roles = await _userManager.GetRolesAsync(user);
         ViewBag.UserRole = roles.FirstOrDefault() ?? "Brak roli";
+
+        //  pobieranie liczników z bazy danych
+        ViewBag.RecipesCount = await _context.Recipes.CountAsync(r => r.UserId == user.Id);
+        ViewBag.CommentsCount = await _context.Comments.CountAsync(c => c.UserId == user.Id);
+        
+        ViewBag.FavoritesCount = 0; 
+
+        var lastActivities = await _context.Recipes
+            .Where(r => r.UserId == user.Id)
+            .OrderByDescending(r => r.Id) // lub po dacie, np. r.CreatedAt jeśli istnieje
+            .Take(3)
+            .ToListAsync();
+
+        ViewBag.LastActivities = lastActivities;
 
         return View(user);
     }
 
-    // formularz edycji
     [HttpGet]
     public async Task<IActionResult> Edit()
     {
@@ -38,13 +56,11 @@ public class ProfileController : Controller
         {
             return NotFound("Nie znaleziono użytkownika.");
         }
-
         return View(user);
     }
 
-    // aktualizacja danych
     [HttpPost]
-    [ValidateAntiForgeryToken] 
+    [ValidateAntiForgeryToken]
     public async Task<IActionResult> Edit(string firstName, string lastName)
     {
         var user = await _userManager.GetUserAsync(User);
@@ -55,7 +71,7 @@ public class ProfileController : Controller
 
         user.FirstName = firstName;
         user.LastName = lastName;
-        
+
         var result = await _userManager.UpdateAsync(user);
         if (result.Succeeded)
         {
